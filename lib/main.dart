3848da0d890d05b575b29645e3131862bec68a2f
ChoiMoonSeok <mon08290@naver.com>
2021-12-31 23:50:22 +0900

    ui 수정

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,8 @@ class FirstPage extends StatelessWidget{
           mainAxisSize: MainAxisSize.max,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+
+
             FloatingActionButton(
               onPressed: (){ // 버튼이 눌리어 졌을 떄
                 // 블루투스 연결 화면으로 이동
@@ -36,6 +38,12 @@ class FirstPage extends StatelessWidget{
               child: Icon(Icons.bluetooth), // 버튼의 모양을 블루투스로 설정
               backgroundColor: Colors.blue, // 버튼의 색을 파랑으로 설정
             ),
+
+            Container(
+              padding: EdgeInsets.all(5),
+              margin: EdgeInsets.all(30),
+            ),
+
             FloatingActionButton(
               onPressed: (){ // 버튼이 눌리어 졌을 떄
                 // 블루투스 연결 화면으로 이동
@@ -76,56 +84,165 @@ class SecondPage extends StatelessWidget{
                 // ble scan
               }
               ),
-
         ],
       ),
     );
   }
 }
 
-class ThirdPage extends StatelessWidget{
+class ThirdPage extends StatelessWidget{ // control 페이지
   @override
+
+  GridView Grid_9(){
+
+    return GridView.count(
+      crossAxisCount: 3, // 1줄에 몇개의 위젯이 들어가는지 결정
+      crossAxisSpacing: 0, // 가로 간격
+      mainAxisSpacing: 35, // 세로 간격
+
+      children: <Widget>[
+
+        Container(
+
+        ),
+
+        Container(
+
+        ),
+
+        Container(
+
+        ),
+
+
+        Container(
+
+          alignment: Alignment.center,
+          padding: const EdgeInsets.all(8),
+          child: FloatingActionButton(
+            onPressed: (){
+
+            },
+            child: Icon(Icons.atm),
+          ),
+        ),
+        //1
+
+        Container(
+
+        alignment: Alignment.center,
+        padding: const EdgeInsets.all(8),
+        child: FloatingActionButton(
+          onPressed: (){
+
+          },
+          child: Icon(Icons.arrow_upward),
+          ),
+        ),//2
+
+
+        Container(
+
+          alignment: Alignment.center,
+          padding: const EdgeInsets.all(8),
+          child: FloatingActionButton(
+            onPressed: (){
+
+            },
+            child: Icon(Icons.atm),
+          ),
+        ), //3
+
+        Container(
+
+          alignment: Alignment.center,
+          padding: const EdgeInsets.all(8),
+          child: FloatingActionButton(
+            onPressed: (){
+
+            },
+            child: Icon(Icons.arrow_left),
+          ),
+
+        ), //4
+
+        Container(
+
+          alignment: Alignment.center,
+          padding: const EdgeInsets.all(8),
+          child: FloatingActionButton(
+            onPressed: (){
+
+            },
+            child: Icon(Icons.atm),
+          ),
+
+        ), //5
+
+        Container(
+
+          alignment: Alignment.center,
+          padding: const EdgeInsets.all(8),
+          child: FloatingActionButton(
+            onPressed: (){
+
+            },
+            child: Icon(Icons.arrow_right),
+          ),
+        ), //6
+
+        Container(
+
+          alignment: Alignment.center,
+          padding: const EdgeInsets.all(8),
+          child: FloatingActionButton(
+            onPressed: (){
+
+            },
+            child: Icon(Icons.light),
+          ),
+        ), // 7
+
+        Container(
+
+          alignment: Alignment.center,
+          padding: const EdgeInsets.all(8),
+          child: FloatingActionButton(
+            onPressed: (){
+
+            },
+            child: Icon(Icons.arrow_downward),
+          ),
+        ), //8
+
+        Container(
+
+          alignment: Alignment.center,
+          padding: const EdgeInsets.all(8),
+          child: FloatingActionButton(
+            onPressed: (){
+
+            },
+            child: Icon(Icons.speaker),
+          ),
+
+        ), // 9
+
+      ],
+
+    );
+  }
+
+
+
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
         title: Text('Control Beeboy'),
       ),
-      body: Row(
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          FloatingActionButton(
-            onPressed: (){ // 버튼이 눌리어 졌을 떄
-            //블루투스를 통해 문자를 전송.
-              },
-            child: Icon(Icons.arrow_upward_sharp), // 버튼의 모양을 게임패로 설정
-            backgroundColor: Colors.cyan, // 버튼의 색을 파랑으로 설정
-          ),
-          FloatingActionButton(
-            onPressed: (){ // 버튼이 눌리어 졌을 떄
-              //블루투스를 통해 문자를 전송.
-            },
-            child: Icon(Icons.arrow_downward_sharp), // 버튼의 모양을 게임패로 설정
-            backgroundColor: Colors.cyan, // 버튼의 색을 파랑으로 설정
-          ),
-          FloatingActionButton(
-            onPressed: (){ // 버튼이 눌리어 졌을 떄
-              //블루투스를 통해 문자를 전송.
-            },
-            child: Icon(Icons.arrow_right), // 버튼의 모양을 게임패로 설정
-            backgroundColor: Colors.cyan, // 버튼의 색을 파랑으로 설정
-          ),
-          FloatingActionButton(
-            onPressed: (){ // 버튼이 눌리어 졌을 떄
-              //블루투스를 통해 문자를 전송.
-            },
-            child: Icon(Icons.arrow_left), // 버튼의 모양을 게임패로 설정
-            backgroundColor: Colors.cyan, // 버튼의 색을 파랑으로 설정
-          ),
+      body: Grid_9(),
+      );
 
-        ],
-      ),
-    );
+
   }
 }
